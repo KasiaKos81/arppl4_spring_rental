3b26133d22f7a5ebc1b7ad3d2cee8ced31922a6a
Kasia <kasiakos81@gmail.com>
2022-07-07 17:50:27 +0200

    first commit

---
+++ src/test/java/pl/sda/arppl4/springrental/Arppl4SpringRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl4.springrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arppl4SpringRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
