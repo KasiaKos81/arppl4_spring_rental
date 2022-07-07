2de1942fd9167730d3873b4df40c70d9e007ff00
Kasia <kasiakos81@gmail.com>
2022-07-07 18:35:19 +0200

    070722

--- src/main/java/pl/sda/arppl4/springrental/controller/CarController.java
+++ src/main/java/pl/sda/arppl4/springrental/controller/CarController.java
@@ -2,9 +2,12 @@ package pl.sda.arppl4.springrental.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.arppl4.springrental.model.Car;
 import pl.sda.arppl4.springrental.service.CarService;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -14,6 +17,27 @@ import pl.sda.arppl4.springrental.service.CarService;
 public class CarController {
 
     private final CarService carService;
+
+    @GetMapping("list/")
+    public List<Car> getAllCars(){
+        log.info("Get the list of all cars");
+        List<Car> list = carService.getAllCars();
+        return list;
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCar(@RequestBody Car car){
+        log.info("Post a car " + car);
+        carService.addCar(car);
+    }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/pl/sda/arppl4/springrental/service/CarService.java
+++ src/main/java/pl/sda/arppl4/springrental/service/CarService.java
@@ -3,7 +3,10 @@ package pl.sda.arppl4.springrental.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.sda.arppl4.springrental.model.Car;
 import pl.sda.arppl4.springrental.repository.CarRepository;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,5 +15,10 @@ import pl.sda.arppl4.springrental.repository.CarRepository;
 public class CarService {
 
     private final CarRepository carRepository;
+
+    public void addCar(Car car) {carRepository.save(car);}
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
 
 }
